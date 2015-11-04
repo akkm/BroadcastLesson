2ea3515585f9b37c6d298a9c4adde46ecb34731f
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2015-11-04 12:25:39 +0900

    remove todos

--- app/src/main/java/com/example/broadcastlesson/MainActivity.java
+++ app/src/main/java/com/example/broadcastlesson/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.broadcastlesson;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyReceiver mReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,25 +13,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO 2 "ACTION_SCHOO" のBroadcastを受信開始する
-        IntentFilter filter = new IntentFilter("ACTION_SCHOO");
-        mReceiver = new MyReceiver();
-        registerReceiver(mReceiver, filter);
     }
 
     @Override
     protected void onDestroy() {
         // TODO 3 受信を終了する
-        unregisterReceiver(mReceiver);
+
+
         super.onDestroy();
     }
 
     // TODO 1 TextViewに "Message" を表示するクラス MyReceiver を作成する
-    private class MyReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String message = intent.getStringExtra("Message");
-            TextView textView = (TextView) findViewById(R.id.message);
-            textView.setText(message);
-        }
-    }
 }
